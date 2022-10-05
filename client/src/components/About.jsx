c5795b16d4c32466d4f3c064f61000e7fbd4bd2a
Karolina Redden <dubajkaro@gmail.com>
2022-10-05 21:00:31 +0000

    include a picture

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,7 +1,10 @@
+import karo from '../karo.jpeg';
+
 export default function About () {
   return (
     <div className="about" id="about">
        <h2>Me, Myself and I</h2>
+       <img src={karo} alt="karo" /> 
         <p>Hi, my name is Karolina and I am a full stack web developer living in Middle Sackville, Nova Scotia. My passion for a technology has pushed me to pivot from being a corporate coach and explore the depths of software development.</p>
         <p>I have graduated from LightHouse Labs Software Development Programe with the following stack expertise:</p>
           <ul>
