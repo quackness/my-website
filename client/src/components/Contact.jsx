74260a8cc3b25108fe6bec3283a1725409ae1b05
Karolina Redden <dubajkaro@gmail.com>
2022-10-05 20:21:04 +0000

    add contact info

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,8 +1,10 @@
 export default function Contact () {
   return (
-    <div>
-      email: xxxx
-      phone: xxx
+    <div className="contact">
+      <h2>Get in touch</h2>
+      <a href="mailto:reddenkarolina@gmail.com">Send Email</a>
+      <a href="https://www.linkedin.com/in/karolina-redden/">LinkedIn</a>
+      <a href="https://github.com/quackness">GitHub</a>
     </div>
 
   )
