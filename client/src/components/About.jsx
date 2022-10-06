6f8145af4d9784e5b83e8695a36a05883b61f652
Karolina Redden <dubajkaro@gmail.com>
2022-10-06 17:44:14 +0000

    style the text and a picture

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,19 +5,17 @@ export default function About () {
     <div className="about" id="about"> 
        <h2>Me, Myself and I</h2>
        <div className="flex-container">
-        <img src={karo} alt="karo" /> 
-          <p>Hi, my name is Karolina and I am a full stack web developer living in Middle Sackville, Nova Scotia. My passion for a technology has pushed me to pivot from being a corporate coach and explore the depths of software development.</p>
-          <p>I have graduated from LightHouse Labs Software Development Programe with the following stack expertise:</p>
+        <img className="flex-left" src={karo} alt="karo" /> 
+        <div className='flex-right'>
+          <p>Hi, my name is Karolina and I am a full stack web developer living in Middle Sackville, Nova Scotia. My passion for a technology has pushed me to pivot from being a corporate coach and explore the depths of software development.
+          I have graduated from LightHouse Labs Software Development Programe with the following stack expertise:</p>
             <ul>
-              Languages: JavaScript, HTML+CSS, Ruby
-            </ul>
-            <ul>
-              Database: SQL, PostgreSQL, Relational Databases
-            </ul>
-            <ul>
-              Frameworks: React.js 
+              <li>Languages: JavaScript, HTML+CSS, Ruby</li>
+              <li>Database: SQL, PostgreSQL, Relational Databases</li>
+              <li>Frameworks: React.js</li>
             </ul>
           <p>When not coding you can find me hiking with my family, at the gym or exploring new restaurats in the city.</p>
+        </div>
       </div>
 
     </div>
