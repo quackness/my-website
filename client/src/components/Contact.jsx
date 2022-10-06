a900ba7faff7f62e1a32117d6cad751465502675
Karolina Redden <dubajkaro@gmail.com>
2022-10-06 18:32:04 +0000

    add icons to contact section

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,10 +1,14 @@
 export default function Contact () {
   return (
-    <div className="contact">
+    <div className="contact" id="contact">
       <h2>Get in touch</h2>
-      <a href="mailto:reddenkarolina@gmail.com">Send Email</a>
+      
+      <a href="mailto:reddenkarolina@gmail.com">Send Email: reddenkarolina@gmail.com</a>
+      <i class="icon-envelope icon-2x"></i>
       <a href="https://www.linkedin.com/in/karolina-redden/">LinkedIn</a>
+      <i class="icon-linkedin icon-2x"></i>
       <a href="https://github.com/quackness">GitHub</a>
+      <i class="icon-github icon-2x"></i>
     </div>
 
   )
