ecb8288b7bcdda67a5a4d14ce90f08dfc1981738
Karolina Redden <dubajkaro@gmail.com>
2022-10-06 20:40:20 +0000

    edit project descriptions

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,10 +1,17 @@
 export default function Portfolio () {
   return (
-    <div>
+    <div className="portfolio" id="portfolio">
       <h2>Projects</h2>
-      <p>ToolSwap</p>
-      <p>Scheduler</p>
-      <p>Tweeter</p>
+        <h3>ToolSwap</h3>
+        <p>Web CRUD app for scheduling a neighbourhood tool rental. The app is using React calendar for logged-in users to schedule the rental.</p>
+        <p> Stack: PostgreSQL, Express, React, Node.</p>
+        <h3>Scheduler</h3>
+        <p>Schedule, edit and delete technical interviews.</p>
+        <p>Back-end: Express, SQL, Node</p>
+        <p>Front-end: React, HTML + CSS</p>
+        <p>Test: Cypress, Jest, StoryBook</p>
+        <h3>Tweeter</h3>
+        <p>Tweeter is a single-page Twitter clone, it uses Ajax to provide a real time update the page with user submitted tweets. The page events are handled with jQuery; the design is powered with HTML + CSS with an emphasis on mobile responsiveness.</p>
     </div>
   )
 }
