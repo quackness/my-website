bdc2d2e0179a1cf9027af709113bc66a5ecf9600
Karolina Redden <dubajkaro@gmail.com>
2022-10-06 19:01:45 +0000

    style the get in touch section

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,13 +2,14 @@ export default function Contact () {
   return (
     <div className="contact" id="contact">
       <h2>Get in touch</h2>
-      
-      <a href="mailto:reddenkarolina@gmail.com">Send Email: reddenkarolina@gmail.com</a>
-      <i class="icon-envelope icon-2x"></i>
-      <a href="https://www.linkedin.com/in/karolina-redden/">LinkedIn</a>
-      <i class="icon-linkedin icon-2x"></i>
-      <a href="https://github.com/quackness">GitHub</a>
-      <i class="icon-github icon-2x"></i>
+      <div className="contact-icons">
+        <i class="icon-envelope icon-2x"></i>
+        <a href="mailto:reddenkarolina@gmail.com">reddenkarolina@gmail.com</a>
+        <i class="icon-linkedin icon-2x"></i>
+        <a href="https://www.linkedin.com/in/karolina-redden/">LinkedIn</a>
+        <i class="icon-github icon-2x"></i>
+        <a href="https://github.com/quackness">/quackness</a>
+      </div>
     </div>
 
   )
