bcbe14cac2c93fa588fcaafae50c710f88021c6e
Karolina Redden <dubajkaro@gmail.com>
2022-10-07 00:07:03 +0000

    add icon for linking a project

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,10 +1,11 @@
 export default function Portfolio () {
   return (
     <div className="portfolio" id="portfolio">
-      <h2>Projects</h2>
-        <h3>ToolSwap</h3>
+      <h2>Projects</h2> 
+        <h3><a href="https://toolswap.netlify.app/">ToolSwap <i class="icon-link"></i></a></h3>
         <p>Web CRUD app for scheduling a neighbourhood tool rental. The app is using React calendar for logged-in users to schedule the rental.</p>
         <p> Stack: PostgreSQL, Express, React, Node.</p>
+        <iframe className="videos" width="660" height="415" src="https://www.youtube.com/embed/Tk4Sc80ZBsE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <h3>Scheduler</h3>
         <p>Schedule, edit and delete technical interviews.</p>
         <p>Back-end: Express, SQL, Node</p>
