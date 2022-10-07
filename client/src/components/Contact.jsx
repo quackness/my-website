ba00ff5487c67233d4ed657f6eafb376976350be
Karolina Redden <dubajkaro@gmail.com>
2022-10-07 00:07:26 +0000

    add styling to contact info

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -3,12 +3,13 @@ export default function Contact () {
     <div className="contact" id="contact">
       <h2>Get in touch</h2>
       <div className="contact-icons">
-        <i class="icon-envelope icon-2x"></i>
+        <span className="contact-element"><i class="icon-envelope icon-2x"></i>
         <a href="mailto:reddenkarolina@gmail.com">reddenkarolina@gmail.com</a>
-        <i class="icon-linkedin icon-2x"></i>
-        <a href="https://www.linkedin.com/in/karolina-redden/">LinkedIn</a>
-        <i class="icon-github icon-2x"></i>
-        <a href="https://github.com/quackness">/quackness</a>
+        </span>
+        <span className="contact-element"><i class="icon-linkedin icon-2x"></i>
+        <a href="https://www.linkedin.com/in/karolina-redden/">LinkedIn</a></span>
+        <span className="contact-element"><i class="icon-github icon-2x"></i>
+        <a href="https://github.com/quackness">/quackness</a></span>
       </div>
     </div>
 
