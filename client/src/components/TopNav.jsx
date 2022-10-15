a1dc59f970c31957bc827b859fa29afc968da968
Karolina Redden <dubajkaro@gmail.com>
2022-10-15 19:19:12 +0000

    fix padding

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -4,9 +4,11 @@ import Portfolio from "./Portfolio";
 export default function TopNav() {
   return (
     <div className="topnav">
+      <div className="nav-text">
        <a href="#contact">Contact</a>
        <a href="#portfolio">Portfolio</a>
       <a href="#about">About</a>
+      </div>
     </div>
   );
 }
