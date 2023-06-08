cffa13d44378b7e58d5bc5b9c2ebd5578f1e8262
Karolina Redden <dubajkaro@gmail.com>
2023-06-07 23:13:06 -0300

    update web site

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,12 +4,17 @@ export default function About() {
   return (
     <div className="about">
       <div class>
-
         <h2 id="about">Me, Myself and I</h2>
       </div>
       <div className="about-container">
         <div className="about-image">
-          <img height="100%" width="100%" className="img-fluid border border-dark" src={karo} alt="karo" />
+          <img
+            height="100%"
+            width="100%"
+            className="img-fluid border border-dark"
+            src={karo}
+            alt="karo"
+          />
         </div>
         <div className="about-right">
           <p>
@@ -17,26 +22,27 @@ export default function About() {
             living in Middle Sackville, Nova Scotia. My passion for technology
             has pushed me to pivot from being a corporate coach and explore the
             depths of software development. I have graduated from LightHouse
-            Labs Software Development Program with the following stack
-            expertise:
+            Labs Software Development Program as well as Skills For Hire with
+            the following stack expertise:
           </p>
           <ul>
             <li>
-              <b>Languages:</b> JavaScript, HTML+CSS, Ruby
+              <b>Languages:</b> JavaScript, PHP, HTML+CSS, Ruby
             </li>
             <li>
               <b>Databases and CMS:</b> SQL, PostgreSQL, Relational Databases,
               WordPress
             </li>
             <li>
-              <b>Frameworks, libraries and enviroments:</b> React, Node.js,
+              <b>Frameworks, libraries and enviroments:</b> React, Vue, Node.js,
               AJAX, jQuery, Express, SASS, EJS, JSX, Sinatra
             </li>
             <li>
               <b>Testing:</b> Mocha, Chai, Cypress, JEST, StoryBook
             </li>
             <li>
-              <b>Tracking and collaboration:</b> JIRA, Confluence, GitHub, Git
+              <b>Tracking and collaboration:</b> JIRA, Confluence, GitHub, Git,
+              Shortcut, Figma
             </li>
           </ul>
           <p>
