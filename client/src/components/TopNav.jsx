07073268029482b4a656741618f32ea9a5dbcdd2
Karolina Redden <dubajkaro@gmail.com>
2023-10-23 15:08:27 -0300

    start the Resume component

--- client/src/components/TopNav.jsx
+++ client/src/components/TopNav.jsx
@@ -1,11 +1,13 @@
 import About from "./About";
 import Portfolio from "./Portfolio";
+import Resume from "./Resume";
 
 export default function TopNav() {
   return (
     <div className="topnav">
       <div className="nav-text">
        <a href="#contact">Contact</a>
+       <a href="/Resume">Resume</a>
        <a href="#portfolio">Portfolio</a>
       <a href="#about">About</a>
       </div>
